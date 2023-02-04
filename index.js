c8b20026e2aad5407320f904379933512cc077a3
Stabby <0xStabby@protonmail.com>
2023-02-03 18:43:07 -0600

    added /tiktok command

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const complements = require('./complements.js');
+const videos = require('./videos.js');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -11,4 +12,13 @@ const bot = new TelegramBot(token, { polling: true });
 bot.onText(/\/joe/, (msg) => {
   const randomIndex = Math.floor(Math.random() * compliments.length);
   bot.sendMessage(msg.chat.id, compliments[randomIndex]);
+});
+
+bot.onText(/\/tiktok/, (msg) => {
+    const randomIndex = Math.floor(Math.random() * videos.length);
+    const video = videos[randomIndex];
+    bot.sendVideo(msg.chat.id, video.filePath).then(() => {
+        bot.sendMessage(msg.chat.id, `${video.link}`);
+        bot.sendMessage(msg.chat.id, "Don't forget to like, comment, and share with your friends!! 🔥🎥");
+    });
 });
---
+++ videos.js
@@ -0,0 +1,20 @@
+const videos = [
+    {
+        filePath: "./videos/7193854790315593002.mp4",
+        link: "https://www.tiktok.com/@arbinucoin_official/video/7193854790315593002"
+    },
+    {
+        filePath: "./videos/7194599222799535406.mp4",
+        link: "https://www.tiktok.com/@arbinucoin_official/video/7194599222799535406"
+    },
+    {
+        filePath: "./videos/7195678476865686827.mp4",
+        link: "https://www.tiktok.com/@arbinucoin_official/video/7195678476865686827"
+    },
+    {
+        filePath: "./videos/7196052952732192042.mp4",
+        link: "https://www.tiktok.com/@arbinucoin_official/video/7196052952732192042"
+    }
+];
+
+module.exports = videos;
